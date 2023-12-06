2d91e519f7054b70824312a041f6c1b17c3e93d8
voxold <bilalajaha@gmail.com>
2023-12-06 21:50:22 +0000

    Python Programming

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
-def add_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d}".format(n))
-        return
-    else:
-        i = 1
-        add = 0
-        while i <= n:
-            add += int(argv[i])
-            i += 1
-        print("{:d}".format(add))
-
+from sys import argv
 if __name__ == "__main__":
-    import sys
-    add_arg(sys.argv)
+    len = len(argv)
+    sum = 0
+    for i in range(1, len):
+        sum += int(argv[i])
+    print("{}".format(sum))
