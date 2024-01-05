7272748471ebf0094e938b3fe2046bba7bf3022d
Voxold <bilalajaha@gmail.com>
2024-01-05 04:02:14 -0800

    Python Programming

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,6 @@
+def add_integer(a, b=98):
+    if not isinstance (a, (int, float)):
+	    raise TypeError("a must be an integer")
+    elif not isinstance (b, (int, float)):
+	    raise TypeError("b must be an integer")
+    return (int(a) + int(b))
