f514e20f647f67ab50ec3e4013a7f40d52c683b9
Voxold <bilalajaha@gmail.com>
2024-02-13 22:58:45 -0800

    city

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+-- Display top 3 cities during July and August
+
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` IN (7, 8)
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
+LIMIT 3;
