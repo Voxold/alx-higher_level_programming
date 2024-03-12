813bfaf7b6276dece821d4ea9b4e5fae3b05f65a
Voxold <bilalajaha@gmail.com>
2024-03-12 07:31:32 -0700

    task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
 if (isNaN(process.argv[2])) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 }
 let num = process.argv[2] | 0;
+let cnt = num;
+const arr = [];
+while (cnt > 0 && cnt--) {
+  arr.push('X');
+}
 while (num > 0 && num--) {
-  console.log('C is fun');
+  console.log(arr.join(''));
 }
