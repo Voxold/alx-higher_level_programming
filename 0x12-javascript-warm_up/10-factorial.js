af840565a50a7c653890261b17f577aa27205761
Voxold <bilalajaha@gmail.com>
2024-03-12 07:09:55 -0700

    task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+function factorial (num) {
+  if (isNaN(num) || num === 0 || num === 1) { return (1); }
+  return (num * factorial(num - 1));
+}
+console.log(factorial(process.argv[2]));
