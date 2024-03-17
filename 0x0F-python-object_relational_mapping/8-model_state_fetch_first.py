82d58224ce12872b8f7fabf5874afcd0408283bd
Voxold <bilalajaha@gmail.com>
2024-03-17 16:17:47 -0700

    task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""class definition of a State"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = session.query(State).first()
+
+    if (result):
+        print("{}: {}".format(result.id, result.name))
+    else:
+        print("Nothing")
