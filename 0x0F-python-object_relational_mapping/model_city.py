5c3fd7c16e8c022a0c76100482cd89d3f5289d0c
Voxold <bilalajaha@gmail.com>
2024-03-17 16:28:51 -0700

    task 15

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""class definition of a State"""
+
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class City(Base):
+    """City class"""
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(128), ForeignKey('states.id'), nullable=False)
