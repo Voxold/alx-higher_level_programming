db3d40f7ec6224171e233cda504c42309acbb4e7
Voxold <bilalajaha@gmail.com>
2024-03-17 16:27:16 -0700

    task 14

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""class definition of a State"""
+
+import sys
+from model_city import Base, City
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = session.query(City, State).order_by(City.id).filter(
+        City.state_id == State.id).all()
+
+    for row in result:
+        print("{}: ({}) {}".format(row.State.name, row.City.id, row.City.name))
